4fea08da15073d9041679e623f6304a6db181610
Light <101825304+Light-Yagami-01@users.noreply.github.com>
2024-09-29 00:54:45 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,26 @@
+// creating a array from user and printing theam
+
+#include <stdio.h>
+void main{
+
+int a[50],i,size;
+
+printf("enter the size of an array:\n");
+scanf("%d",&size);
+
+printf("enter the array:\n");
+for(i=0;i<size;i++){
+   scanf("%d",%a[i]);
+}
+
+printf(i);
+
+
+
+
+
+
+
+
+
+}
